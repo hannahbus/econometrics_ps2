fa984253990624e6cb53f71daa8ea3818dfa53ff
pqheinemann <38588006+pqheinemann@users.noreply.github.com>
2018-04-21 16:54:57 +0200

    Add files via upload

--- ps2.R
+++ ps2.R
@@ -1,1 +1,13 @@
-1+1
+library(psych)
+
+#Part 1====
+#1
+evs = read.csv("ps2_EVS.csv")
+describe(evs)
+summary(evs$country)
+str(evs$country)
+str(evs$Unemployed)
+summary(evs$Unemployed)
+str(evs$Sex)
+summary(evs$Education)
+hist(evs$Education)
