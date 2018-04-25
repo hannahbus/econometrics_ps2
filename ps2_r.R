cfb36cb0ff4d938c933ba250e0fbd21f8de79564
hannahbus <38669089+hannahbus@users.noreply.github.com>
2018-04-25 20:39:59 +0200

    Update ps2_r.R

--- ps2_r.R
+++ ps2_r.R
@@ -8,14 +8,9 @@ library(Matrix)
 library(lmtest)
 library(sandwich)
 library(skimr)
-#to do====
-#2.7 clustered errors manually?
-#part3(optional)
-
 
 #Part 1====
 #1 summary
-#Alternativ nur skim(evs) berücksichtigt die Datenstruktur 
 
 evs = read.csv("ps2_EVS.csv")
 describe(evs)
@@ -25,8 +20,7 @@ skim(evs)
 # Latter fact may explain why there are some observations with no elementary-education. Also in accordance, with the sampling structure reported ages look also plausible spreading from 15 to 
 #108 years. With fewer observations at the lower and upper tails. 
 #No apparent mistakes in data. Self-reported values of happiness and satisfaction lie within the 1-4 and 1-10 range. 
-#Dummy variables take on two values - 0,1, year only 2008. 
-#Hannah Gender distribution, education distribution etc. 
+#Dummy variables take on two values - 0,1, year only 2008.  
 
 #2 Conditional Expectation
 cond_exp_age = aggregate(evs$Satisfaction, list(evs$Age), mean)
@@ -60,13 +54,16 @@ summary(reg2)
 #Effect how much unemployment decreases self-reported satisfaction depends on whether the partner is unemployed or not.
 #If partner is unemployed, unemployment only decreases satisfaction by  (0.8725 - 0.3976) = 0.4749 points ceteris paribus.
 #If partner isn't unemployed, unemployment decreases satisfaction by 0.8725 points ceteris paribus. 
-# Hannah To Do: Beta1 und co Beziehung einfügen. 
+
 
 #6
-coeftest(reg2, vcov = vcovHC(reg2, type = "HC0")) #To Do Hannah: Check this command produces same results, used sandwich command 
+coeftest(reg2, vcov = vcovHC(reg2, type = "HC0")) 
 #If the error terms are heteroskedastic, OLS standard errors are incorrect. White standard allow for heteroskedasticity and yield correctly computed standard errors and thus correct conclusions about the significance of estimated coefficients.  
-#When interpreting estimated coefficients one should keep in mind that the dependend variable is self-reported satisfaction in a survey and not a naturally measured variable. Thus one should for example refrain from intepretations like "a person with satisfaction 8 is twice as happy as one with satisfaction 4."
-#Hannah To Do: Argumentieren, grosse Abweichungen?
+#When interpreting estimated coefficients one should keep in mind that the dependend variable is self-reported satisfaction 
+#in a survey and not a naturally measured variable. Thus one should for example refrain from intepretations like "a person with 
+# satisfaction 8 is twice as happy as one with satisfaction 4." In fact, a multinomial probit model would also be a good choice to 
+# examine the influence of certain regressors on the self-reported happiness.
+
 
 #7
 #H0 : beta1*beta2 = beta3 using delta method
@@ -107,7 +104,8 @@ penn = read.csv("ps2_Penn.csv")
 #1 
 #Contribute to the debate on the true costs of business cycles. 
 #Run regressions on self-reported satisfaction of thousands of people from different countries and find: 
-#(1) Broadly common structure of happiness regressions across countries, i.e. a sizable set of personal characteristics has a similar influence on self-reported satisfaction
+#(1) Broadly common structure of happiness regressions across countries, i.e. a sizable set of personal characteristics has a similar 
+#influence on self-reported satisfaction
 #(2) Business cycles have a sizable effects on self-reported satisfaction. In monetary terms: Give each citizen with 200 dollars
 # per year to compensate for induced losses in self-reported satisfaction. 
 
@@ -116,17 +114,19 @@ penn = read.csv("ps2_Penn.csv")
 evs = read.csv("ps2_EVS.csv")
 merged = merge(evs, penn, by = c("country", "year"))
 # less observations. for some country in evs there was no information in penn
-# reason 1: evs confined to European countries, penn not. Some countries such as Northern Ireland (Hannah include uni notes) not included in penn.
+# reason 1: evs confined to European countries, penn not. 
+# reason 2: Kosovo, Macedonia, Moldova, Northern Cyprus, Northern Ireland in evs but not in penn. 
 
 #3 Conditional Expectation
 #unemployed
 cond_exp_empU = aggregate(merged$Satisfaction[merged$Unemployed == 1], list(merged$empPopRatio[merged$Unemployed == 1]), mean)
 plot(cond_exp_empU$Group.1, cond_exp_empU$x, "b")
-#hardly any relationship visually detectable #Hannah: causal/linear, more or less spreads randomly?
+
+#hardly any relationship visually detectable
 #employed
 cond_exp_empE = aggregate(merged$Satisfaction[merged$Unemployed == 0], list(merged$empPopRatio[merged$Unemployed == 0]), mean)
 plot(cond_exp_empE$Group.1, cond_exp_empE$x, "b")
-#difficult to see relationship, but it seems like satisfaction is increasing in empPopRatio.
+#satisfaction seems to be increasing in empPopRatio.
 
 #4
 reg1 = lm(Satisfaction ~ Unemployed + empPopRatio, data = merged)
@@ -191,26 +191,6 @@ moultonSE
 comparison = data.frame(robust=robustSE, moulton=moultonSE, clustered=clusteredSE)
 comparison
 
-#manually
-#need for each cluster g: X_g and Psi_g
-merged = arrange(merged, index)
-reg1 = lm(Satisfaction ~ Unemployed + empPopRatio, data = merged)
-resid = reg1$residuals
-X = model.matrix(~., data = merged %>% select(Unemployed, empPopRatio, index))
-X[,-4]
-X = data.frame(X)
-sum = matrix(0,nrow = 3, ncol = 3)
-for (i in 1:41) {
-  sum = sum + 
-    t(cbind(X$X.Intercept.[X$index == i], X$Unemployed[X$index == i], X$empPopRatio[X$index ==i])) %*%
-    ((length(unique(X$index))/(length(unique(X$index)) - 1)) * (merged$residuals[merged$index == i] %*% t(merged$residuals[merged$index == i]))) %*%
-    cbind(X$X.Intercept.[X$index == i], X$Unemployed[X$index == i], X$empPopRatio[X$index == i])
-}
-#sum should be sum over g of (X_g)' times Psi_hat times X_g in formula from slide 23
-clustervhatbeta = solve(t(X[,-4]) %*% X[,-4]) %*% sum %*% solve(t(X[,-4]) %*% X[,-4])
-#not sure why it isn't working
-#Hannah: Check uni 
-
 #8
 reg2 = lm(Satisfaction ~ Unemployed + empPopRatio + Unemployed:empPopRatio, data = merged)
 summary(reg2)
@@ -218,6 +198,8 @@ coeftest(reg2, vcov=vcovHC(reg2, type = "HC0", cluster = "index"))
 #coefficient on unemployment positive. However beta1 is not the whole effect of Unemployment on self-reported satisfaction. Due to the interaction term it also depends on the value of empPopRatio. The estimated coefficient for the interaction term is negative. Taking the derivative of the regression equation with the estimated coefficients shows that the expected marginal effect of Unemployment (including the effect through the interaction term) is negative if empPopRatio is larger than beta1/(-beta3) = 1.14/5 = 0.228.
 
 #9
-curve(reg2$coefficients[1] + reg2$coefficients[2] + reg2$coefficients[3]*x + reg2$coefficients[4]*x, from = 0, to = 1)
-# for unemployed people, empPopRatio negatively affects Satisfaction. Conversely, for an employed person satisfaction inreases c.p. in empPopRatio.
-
+curve(reg2$coefficients[1] + reg2$coefficients[2] + reg2$coefficients[3]*x + reg2$coefficients[4]*x, from = 0, to = 1, col= "red")
+curve(reg2$coefficients[1] +  reg2$coefficients[3]*x, add=TRUE, col ="green")
+legend("bottomleft", legend=c("Unemployed", "Employed"), col=c("red", "green"), rgb("red", "green"), pch = c(17,19), bty = "n", pt.cex = 2, cex = 1.2, text.col = "black", horiz =F, inset = c(0.1, 0.1))
+# for unemployed people, empPopRatio negatively affects self-reported satisfaction. Conversely, for an employed person satisfaction inreases c.p. in empPopRatio.
+# include code for two-line graph to illustrate difference between employed and unemployed.
